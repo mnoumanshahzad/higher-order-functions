3953baf570e1fdeb00f4356e52e7877454e93d4c
mnoumanshahzad <mnoumanshahzad@hotmail.com>
2016-04-19 13:26:20 -0700

    Reimplemented 'reduce_' using recursion

--- lib/myLib.py
+++ lib/myLib.py
@@ -2,21 +2,27 @@ author = 'mnoumanshahzad'
 
 
 def _map(func_, list_):
-  """
-  Applies the provided function on the specified list
-  :return: List of transformed items
-  """
-  return [func_(item) for item in list_]
+    """
+    Applies the given function to each item of the list
+    :param func_:
+    :param list_:
+    :return: A new list of transformed items
+    """
+    return [func_(item) for item in list_]
 
 
 def _reduce(func_, list_):
-    result = None
-    for i in range(len(list_) - 1):
-        if i == 0:
-            temp = list_[i]
-        else:
-            temp = result
-        result = func_(temp, list_[i + 1])
-    return result
+    """
+    Applies to the list of items to reduce the result to a single item
+    :param func_:
+    :param list_:
+    :return: A single, aggregated item
+    """
+    if len(list_) == 1:
+        return list_[0]
+    else:
+        head = list_.pop(0)
+        aggr = reduce(func_, list_)
+        return func_(head, aggr)
 
 
